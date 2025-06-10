aa42b0bcf83fa284de35ca7598416f6450f6a0f4
Vincenzo Maisto <vincenzo.maisto2@unina.it>
2025-06-10 22:07:10 +0200

    [schedulers] implement round robin

---
+++ schedulers/round_robin.py
@@ -0,0 +1,21 @@
+# Description:
+#    Simple Round Robing scheduler
+
+def thread_allocation_RR (
+                            hw_config_df,
+                            workload_df,
+                            S
+                        ):
+    # s_{d,t} = 1, \ \textrm{if} \ d = t \ mod \ |D|
+    LEN_D = len(hw_config_df)
+    for thread_index, row in workload_df.iterrows():
+        for d_index, row in hw_config_df.iterrows():
+            # NOTE: this could be handled more efficiently with a static
+            #   variable holding the previously assigned NPU.
+            #   Although, the advantage might be minimal in python
+            if d_index == thread_index % LEN_D:
+                S[d_index][thread_index] = 1
+            else:
+                S[d_index][thread_index] = 0
+
+
